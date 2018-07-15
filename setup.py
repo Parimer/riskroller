c5695392679c48b8f8e989c0dda9376b1bc04f1f
James Campbell <james@jamescampbell.us>
2018-07-14 22:59:01 -0400

    updates

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import re
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('risk/risk.py').read(),
+    re.M
+    ).group(1)
+
+setup(
+    name='riskroller',
+    author='James Campbell',
+    author_email='james@jamescampbell.us',
+    version=version,
+    license='GPLv3',
+    description = 'Play one random or inputed initial conditions of a round of Risk.',
+    packages=['riskroller'],
+    py_modules=['riskroller'],
+    keywords = ['risk', 'data-analysis', 'dice', 'statistics', 'die-roll'],
+    classifiers = ["Programming Language :: Python :: 3 :: Only"],
+    install_requires=[
+        'argparse',
+        'pandas',
+        'pprint',
+        'requests',
+        'twitter-scraper',
+        'markovify'
+    ],
+    entry_points={
+        'console_scripts': [
+            'risk = risk.risk:main',
+        ],
+        },
+    url = 'https://github.com/jamesacampbell/tayne',
+    download_url = 'https://github.com/jamesacampbell/tayne/archive/{}.tar.gz'.format(version)
+)
